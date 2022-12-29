550a551252638d3f38f67c8da1f7cffa463bceb7
charandeep.singh <charandeep.singh@mitratech.com>
2022-12-29 20:42:06 +0530

    react_demo_app - react rules, JSX, components, what is new in react 17th version

--- React Crash course/react_demo_app/src/App.js
+++ React Crash course/react_demo_app/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"; // no need to write this in React latest version 17
+// 1. import React from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return(<> 
+            <MyName /> {/* Neted Component -> Calling one component in other component */}
+            <h1 className="JSX">Hello World, Welcome to React Demo Project {3 + 4} </h1>
+            <p>Awesome</p>
+            <img src="" alt="" /> 
+            {/* 
+            Q What is JSX? Rules to write JSX.
+              1. It always return single element
+              2. use Div or React Fragment to wrap all your JSX Code
+              3. ClassName in place of class
+              4. Need to close all non closing Tags in JSX
+              5. Use camelCase for attributes
+            */}
+         </>
+        ); // always return JSX, we need to return anything each time
+};
+
+const MyName = () => { // functional conponent
+  return (<div>
+    <h1>Chanrandeep Singh</h1>
+  </div>);
+};
+
+// // how react is actually looking into it
+// const App = () => {
+//   return React.createElement("div", {}, 
+//             React.createElement("h1", {}, "Hello World"));
+// };
 
 export default App;
+// export default MyName;
