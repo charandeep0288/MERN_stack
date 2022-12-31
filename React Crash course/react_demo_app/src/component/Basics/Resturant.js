98bba0143a7026c84a05fb4a94e79afe4afef585
charandeep0288 <ckaunsel123@gmail.com>
2023-01-01 01:02:28 +0530

    Resturant App - Created component, CSS, JSON, UseState hooks

--- React Crash course/react_demo_app/src/component/Basics/Resturant.js
+++ React Crash course/react_demo_app/src/component/Basics/Resturant.js
@@ -1,25 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import Menu from "./menuApi.js";
+import MenuCard from "./MenuCard";
 
 const Resturant = () => {
-    // const image = "https://offerongrocery.com/wp-content/uploads/2020/09/Maggi-2-Minute-Instant-Noodles-Masala-70g.jpg"
-    return (<>
-                <div className="card-container">
-                    <div className="card"> 
-                        <div className="card-body">
-                            <span className="card-number card-circle subtle">1</span>
-                            <span className="card-author subtle">Breakfast</span>
-                            <h2 className="card-title"> Maggi </h2>
-                            <span className="card-description subtle">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias tenetur sit ducimus delectus assumenda itaque consectetur nihil rem doloremque sint!</span>
-                            <div className="card-read">Read</div>
-                        </div>
+  // UseState Hooks -> use it to manage data
+  const [menuData, setMenuData] = useState(Menu); // OR React.useState(Menu);
+//   console.log(menuData);
 
-                        <img src="https://offerongrocery.com/wp-content/uploads/2020/09/Maggi-2-Minute-Instant-Noodles-Masala-70g.jpg" alt="images" className="card-media" />
 
-                        <span className="card-tag subtle">Order Now</span>
-                    </div>
-                </div>
-            </>);
+  return (
+    <>
+      <MenuCard menuData = {menuData} />
+    </>
+  );
 };
 
-export default Resturant;     
+export default Resturant;
