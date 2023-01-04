8b44cd19a6becdb5fbe6a599e8051b243032e3be
charandeep0288 <ckaunsel123@gmail.com>
2023-01-04 23:12:33 +0530

    TODO APP - removed some files and created its User Interface, basic UI developed

---
+++ React Crash course/todo_list_app/src/component/todo_components/todo.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./style.css";
+
+const Todo = () => {
+  return (
+    <>
+      <div className="main-div">
+        <div className="child-div">
+          <figure>
+            <img src="./images/todo.svg" alt="todologo" />
+            <figcaption>Add Your List Here ✌</figcaption>
+          </figure>
+          <div className="addItems">
+            <input
+              type="text"
+              placeholder="✍ Add Item"
+              className="form-control"
+            />
+            <i className="fa fa-plus add-btn"></i>
+          </div>
+          {/* Show our items */}
+          <div className="showItems">
+            <div className="eachItem">
+              <h3>apple</h3>
+              <div className="todo-btn">
+                <i className="far fa-edit add-btn"></i>
+                <i className="far fa-trash-alt add-btn"></i>
+              </div>
+            </div>
+          </div>
+
+          {/* Remove all button */}
+          <div className="showItems">
+            <button className="btn effect04" data-sm-link-text="Remove All">
+              <span>CHECK LIST</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Todo;
